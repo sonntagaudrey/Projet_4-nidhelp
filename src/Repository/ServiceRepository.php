a04fd50c688e24ac1addf0f173e243062579ee7c
Audrey Connasse <sonntagaudrey@yahoo.fr>
2026-04-06 20:27:34 +0200

    page home

--- src/Repository/ServiceRepository.php
+++ src/Repository/ServiceRepository.php
@@ -16,6 +16,15 @@ class ServiceRepository extends ServiceEntityRepository
         parent::__construct($registry, Service::class);
     }
 
+    public function findLastThree(): array
+    {
+        return $this->createQueryBuilder('s')
+            ->orderBy('s.createDate', 'DESC')
+            ->setMaxResults(3)               
+            ->getQuery()
+            ->getResult();
+    }
+    
 //    /**
 //     * @return Service[] Returns an array of Service objects
 //     */
